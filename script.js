c7102aa6937bc8be01c7c68d3fe2ea6537310ecb
Vardan135 <67186965+Vardan135@users.noreply.github.com>
2020-11-21 12:22:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const panel = document.querySelector("#panel");
 const view = document.querySelector("#view");
 const btn = document.querySelectorAll(".button");
 const content = document.querySelectorAll(".content");
-const aroow = document.querySelectorAll(".arrow");
+const arrow = document.querySelectorAll(".arrow");
 let elements = panel.getElementsByClassName("content");
 let bool = false;
 function open_content(e) {
 	content.forEach(item => { item.style.display = "none" });
-	aroow.forEach(item => { item.src = "images/arrow.png"; item.classList.remove("anim") });
+	arrow.forEach(item => { item.src = "images/arrow.png"; item.classList.remove("anim") });
 	content[e].style.display = "flex";
 	if (window.outerWidth <= 1200) {
-		aroow[e].classList.add("anim");
+		arrow[e].classList.add("anim");
 		view.classList.add("anim2");
 		panel.appendChild(view);
 		btn[e].after(view);
@@ -23,5 +23,5 @@ function open_content(e) {
 			bool = false;
 		}
 	}
-	aroow[e].src = "images/arrow_select.png";
+	arrow[e].src = "images/arrow_select.png";
 }
