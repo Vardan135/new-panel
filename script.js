3a55e082ac98ad669dc8343ffbd7b8c259d45598
Vardan135 <67186965+Vardan135@users.noreply.github.com>
2020-11-21 12:22:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+const panel = document.querySelector("#panel");
+const view = document.querySelector("#view");
 const btn = document.querySelectorAll(".button");
 const content = document.querySelectorAll(".content");
-function a(e) {
+const aroow = document.querySelectorAll(".arrow");
+let elements = panel.getElementsByClassName("content");
+let bool = false;
+function open_content(e) {
 	content.forEach(item => { item.style.display = "none" });
-	content[e].style.display = "block";
+	aroow.forEach(item => { item.src = "images/arrow.png"; item.classList.remove("anim") });
+	content[e].style.display = "flex";
+	if (window.outerWidth <= 1200) {
+		aroow[e].classList.add("anim");
+		view.classList.add("anim2");
+		panel.appendChild(view);
+		btn[e].after(view);
+		bool = true;
+	}
+	else {
+		if (bool) {
+			panel.removeChild(view);
+			document.querySelector("#box").appendChild(view);
+			bool = false;
+		}
+	}
+	aroow[e].src = "images/arrow_select.png";
 }
